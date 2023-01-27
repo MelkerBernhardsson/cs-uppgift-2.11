b046e516ba219119da9ed4e866ebd3e26faf5e19
Pingvu <nhlpool27@gmail.com>
2023-01-27 08:35:45 +0100

    Add project files.

---
+++ cs uppgift 2.11/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[]args)
+        {
+            Console.WriteLine("hur många meter hoppade Alma?");
+            float A = float.Parse(Console.ReadLine());
+            Console.WriteLine("hur många meter hoppade Elin?");
+            float E = float.Parse(Console.ReadLine()); 
+            float dif = E - A;
+            Console.WriteLine("Elin hoppade " + dif + " meter längre än Alma");
+        }
+    }
+}
